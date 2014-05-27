6653a0472849203bec846dee162ce00ecbf62c8e
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-05-27 17:31:45 +0200

    Attempt to improve multiple observables in a list, using zip

--- stats/src/main/Statistics.java
+++ stats/src/main/Statistics.java
@@ -1,48 +1,52 @@
 import rx.Observable;
 import rx.apache.http.ObservableHttpResponse;
+import rx.functions.FuncN;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.OptionalDouble;
-import java.util.concurrent.TimeUnit;
+
+import static java.util.stream.Collectors.toList;
 
 public class Statistics {
 
     public static void main(String[] args) {
 
-        Observable<ObservableHttpResponse> observableRequest1 = Http.getAsync("http://localhost:3001/stream");
-        Observable<ObservableHttpResponse> observableRequest2 = Http.getAsync("http://localhost:3002/stream");
-        Observable<ObservableHttpResponse> observableRequest3 = Http.getAsync("http://localhost:3003/stream");
-        Observable<ObservableHttpResponse> observableRequest4 = Http.getAsync("http://localhost:3004/stream");
+        List<Observable<ObservableHttpResponse>> observableRequests = Arrays.asList(
+                Http.getAsync("http://localhost:3001/stream"),
+                Http.getAsync("http://localhost:3002/stream"),
+                Http.getAsync("http://localhost:3003/stream"),
+                Http.getAsync("http://localhost:3004/stream"));
 
-        Observable<Stats> observableLoad1 = observableRequest1.flatMap(response -> response.getContent().map(new EventStreamJsonMapper<>(Stats.class)));
-        Observable<Stats> observableLoad2 = observableRequest2.flatMap(response -> response.getContent().map(new EventStreamJsonMapper<>(Stats.class)));
-        Observable<Stats> observableLoad3 = observableRequest3.flatMap(response -> response.getContent().map(new EventStreamJsonMapper<>(Stats.class)));
-        Observable<Stats> observableLoad4 = observableRequest4.flatMap(response -> response.getContent().map(new EventStreamJsonMapper<>(Stats.class)));
+        List<Observable<Stats>> observableStats = observableRequests.stream()
+                .map(observableRequest ->
+                        observableRequest.flatMap(response ->
+                                response.getContent()
+                                        .map(new EventStreamJsonMapper<>(Stats.class))))
+                .collect(toList());
 
         Observable<List<Stats>> joinedObservables = Observable.zip(
-                observableLoad1,
-                observableLoad2,
-                observableLoad3,
-                observableLoad4,
+                observableStats.get(0),
+                observableStats.get(1),
+                observableStats.get(2),
+                observableStats.get(3),
                 Arrays::asList);
 
         joinedObservables
-                .take(4)
+                .take(10)
                 .subscribe(
-                    (List<Stats> statslist) -> {
-                        System.out.println(statslist);
+                        (List<Stats> statslist) -> {
+                            System.out.println(statslist);
 
-                        double average = statslist.stream()
-                                .mapToInt(stats -> stats.ongoingRequests)
-                                .average()
-                                .getAsDouble();
+                            double average = statslist.stream()
+                                    .mapToInt(stats -> stats.ongoingRequests)
+                                    .average()
+                                    .getAsDouble();
 
-                        System.out.println("avg: " + average);
-                    },
-                    System.err::println,
-                    Http::shutdown);
+                            System.out.println("avg: " + average);
+                        },
+                        System.err::println,
+                        Http::shutdown);
     }
 
 }
